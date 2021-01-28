50ea431bffefe5a7aad763c535669f5ad1926ef1
udaypatil22@gmail.com <udaypatil22@gmail.com>
2021-01-28 11:58:59 +0000

    fffffffffff

---
+++ democheckigit/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace democheckigit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
